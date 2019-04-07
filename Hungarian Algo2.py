193447321898f04313a476aac679825099496bac
philxchen <cxuankai@outlook.com>
2019-04-07 10:13:47 -0400

    Implement Hungarian Method

--- Hungarian Algo2.py
+++ Hungarian Algo2.py
@@ -53,45 +53,161 @@ def createBipartiteGraph(adjacencyList):
     return g
 
 
-def hungarianAlgo(g):
-    if (len(g.graph['X']) != len(g.graph['Y'])):
-        print("len x != len y impossible to have perfect match")
-    initialMatching(g)  # initial matching
-    perfectMatchBool, unSaturatedX, unSaturatedY = isPerfectMatch(g)
-    stopCondition = False  # N(S) = T
-    while perfectMatchBool == False and stopCondition == False:  # while not perfect match and did not reach stop condition
-        SReachedT = False  # find an vertex for augmenting path
-        s = set()  # init S
-        t = set()  # init Y
-        """
-        我在这停了
-        logic有点问题
-        """
-        while (SReachedT == False):
-            for x in unSaturatedX:  # take a x in unSaturatedX
-                s.add(x)  # put x in s
-                stopCondition, sNeighborNotInT = nsEqualT(g, s, t)  # test for stop condition
-                if stopCondition:  # stop the inner for loop
-                    break
-                print(s)
-                print(t)
-                unSaturatedNeighbor = findUnSaturatedNeighbor(g, x)  # find posible solution
-                print(unSaturatedNeighbor)
-                if unSaturatedNeighbor == None:  # if every neighbor of x saturated
-                    y = sNeighborNotInT.pop()
-                    t.add(y)
-                    s.add(g.nodes[y]['matchingVertex'])
-                    print(s)
-                    print(t)
-            if stopCondition:  # stop inner while
-                break
-            break
-        if stopCondition:  # stop outter while loop
-            break
-        break
+class HungarianAlgorithm:
+    def __init__(self, g):
+        self.g = g
+        self.V = g.nodes
+        self.X = g.graph['X']
+        self.Y = g.graph['Y']
+        self.visited = dict.fromkeys(self.V, False)  # record visited status in constructing M-alternating tree
+        self.mAlternatingTree = nx.Graph()
 
-    colors = [g[u][v]['color'] for u, v in g.edges()]
-    nx.draw(g, nx.bipartite_layout(g, g.graph['x']), with_labels=True, edge_color=colors, width=5)
+    def doesMSaturateX(self):
+        '''
+        Check if M saturates X in g
+        :return: True if M saturates X and False otherwise
+        '''
+        return all(self.V[x]['saturated'] for x in self.X)
+
+    def initialMatching(self):
+        for x in self.X:  # loop through all element of x. x is only the key
+            # g.graph['X'] = {0, 1, 2, 3, 4}
+            for y in self.g[x]:  # loop through all neighbor of x. y is a key
+                if self.V[y]['saturated'] == False:  # find a neighbor that is not saturated
+                    # match x and y
+                    self.V[x]['saturated'] = True
+                    self.V[y]['saturated'] = True
+                    self.V[y]['matchingVertex'] = x
+                    self.V[x]['matchingVertex'] = y
+                    self.g[x][y]['color'] = setEdgeColor(self.g[x][y]['match'],
+                                                         self.g[x][y]['path'])  # set color for visual
+                    break
+
+    def getFirstUnsaturatedX(self):
+        '''
+        Get the first M-unsaturated vertex in X. Return None if there is no such vertex.
+        :return: The first M-unsaturated vertex in X and None if no such vertex exists.
+        '''
+        return next((x for x in self.X if not self.V[x]['saturated']), None)
+
+    def getMAlternatingTree(self, u, needEdgeInM):
+        '''
+        Use DFS to construct an M-alternating tree.
+        :param u: The current vertex
+        :param needEdgeInM: True if the child vertex needs to be M-saturated and False otherwise
+        '''
+        self.visited[u] = True
+        for v in sorted(self.g[u]):
+            if not self.visited[v] and (needEdgeInM == (self.V[u]['matchingVertex'] == v)):
+                self.mAlternatingTree.add_edge(u, v)
+                self.getMAlternatingTree(v, not needEdgeInM)
+
+    def enlargeM(self, u):
+        '''
+        Enlarge M with a (u, y)-path if there is one.
+        :param u: The root of the M-alternating tree
+        '''
+        # T = intersection of V(mAlternatingTree) and Y
+        # T contains an M-unsaturated vertex y different from u.
+        T = sorted(set(self.mAlternatingTree.nodes.keys()) & self.Y)
+        mUnsaturatedY = next((y for y in T if not self.V[y]['saturated']), None)
+        if mUnsaturatedY:  # An M-unsaturated y exists. Enlarge M.
+            uyPath = next(nx.all_simple_paths(self.mAlternatingTree, u, mUnsaturatedY), None)
+            if uyPath:
+                print('The ({}, {}) path is {}'.format(u, mUnsaturatedY, uyPath))
+                # For every two vertices, update the matching properties (saturated, matchingVertex)
+                # of it and the next vertex in the (u, y)-path.
+                for i in range(0, len(uyPath), 2):
+                    if i != 0:
+                        prevEdge = self.g[uyPath[i]][uyPath[i + 1]]
+                        prevEdge['match'] = False
+                        prevEdge['color'] = setEdgeColor(False, False)
+
+                    vProps = self.V[uyPath[i]]
+                    vProps['saturated'] = True
+                    vProps['matchingVertex'] = uyPath[i + 1]
+
+                    nextVProps = self.V[uyPath[i + 1]]
+                    nextVProps['saturated'] = True
+                    nextVProps['matchingVertex'] = uyPath[i]
+
+                    edge = self.g[uyPath[i]][uyPath[i + 1]]
+                    edge['match'] = True
+                    edge['path'] = True
+                    edge['color'] = setEdgeColor(True, True)
+
+                    print('{{{}, {}}} is marked M-saturated'.format(uyPath[i], uyPath[i + 1]))
+
+                # Cleanup the path
+                # TODO: Only cleanup upon user's request
+                for i in range(0, len(uyPath), 2):
+                    edge = self.g[uyPath[i]][uyPath[i + 1]]
+                    edge['path'] = False
+                    edge['color'] = setEdgeColor(edge['match'], False)
+
+    def getMatching(self):
+        '''
+        Get the current matching in G in pairs of vertices.
+        :return: A list of pairs of vertices representing edges in the current matching
+        '''
+        return [{x, self.V[x]['matchingVertex']} for x in sorted(self.X)]
+
+    def start(self):
+        if len(self.X) != len(self.Y):
+            print("|X| != |Y| => impossible to have a perfect matching")
+        self.initialMatching()  # initial matching
+
+        while not self.doesMSaturateX():
+            # Choose the first M-unsaturated vertex u in X and start searching for M-augmenting paths from u
+            u = self.getFirstUnsaturatedX()
+            self.visited = dict.fromkeys(self.V, False)
+            self.getMAlternatingTree(u, False)
+
+            if len(self.mAlternatingTree):
+                self.enlargeM(u)
+                self.mAlternatingTree.clear()
+            else:
+                print('Impossible to have a perfect matching')
+                break
+
+        print('The final matching is')
+        print(self.getMatching())
+
+    # perfectMatchBool, unSaturatedX, unSaturatedY = isPerfectMatch(g)
+    # stopCondition = False  # N(S) = T
+    # while perfectMatchBool == False and stopCondition == False:  # while not perfect match and did not reach stop condition
+    #     SReachedT = False  # find an vertex for augmenting path
+    #     s = set()  # init S
+    #     t = set()  # init Y
+    #     """
+    #     我在这停了
+    #     logic有点问题
+    #     """
+    #     while (SReachedT == False):
+    #         for x in unSaturatedX:  # take a x in unSaturatedX
+    #             s.add(x)  # put x in s
+    #             stopCondition, sNeighborNotInT = nsEqualT(g, s, t)  # test for stop condition
+    #             if stopCondition:  # stop the inner for loop
+    #                 break
+    #             print(s)
+    #             print(t)
+    #             unSaturatedNeighbor = findUnSaturatedNeighbor(g, x)  # find possible solution
+    #             print(unSaturatedNeighbor)
+    #             if unSaturatedNeighbor == None:  # if every neighbor of x saturated
+    #                 y = sNeighborNotInT.pop()
+    #                 t.add(y)
+    #                 s.add(g.nodes[y]['matchingVertex'])
+    #                 print(s)
+    #                 print(t)
+    #         if stopCondition:  # stop inner while
+    #             break
+    #         break
+    #     if stopCondition:  # stop outter while loop
+    #         break
+    #     break
+
+    # colors = [g[u][v]['color'] for u, v in g.edges()]
+    # nx.draw(g, nx.bipartite_layout(g, g.graph['X']), with_labels=True, edge_color=colors, width=5)
 
 
 def findUnSaturatedNeighbor(g, x):
@@ -112,21 +228,6 @@ def nsEqualT(g, s, t):
         for y in g[x]:
             sNeighbor.add(y)
     return (sNeighbor == t, sNeighbor - t)
-
-
-def initialMatching(g):
-    for x in g.graph['X']:  # loop through all element of x. x is only the key
-        # g.graph['x'] = {0, 1, 2, 3, 4}
-        for y in g[x]:  # loop through all neighbor of x. y is a key
-            if g.nodes[y]['saturated'] == False:  # find a neighbor that is not saturated
-                # match x and y
-                g.nodes[x]['saturated'] = True
-                g.nodes[y]['saturated'] = True
-                g.nodes[y]['matchingVertex'] = x
-                g.nodes[x]['matchingVertex'] = y
-                g[x][y]['match'] = True
-                g[x][y]['color'] = setEdgeColor(g[x][y]['match'], g[x][y]['path'])  # set color for visual
-                break
 
 
 def setEdgeColor(match, path):
@@ -151,10 +252,10 @@ def isPerfectMatch(g):
     unSaturatedX = []
     unSaturatedY = []
     perfect = False
-    for x in g.graph['x']:  # find all unSaturatedX
+    for x in g.graph['X']:  # find all unSaturatedX
         if g.nodes[x]['saturated'] == False:
             unSaturatedX.append(x)
-    for y in g.graph['y']:  # find all unSaturatedY
+    for y in g.graph['Y']:  # find all unSaturatedY
         if g.nodes[y]['saturated'] == False:
             unSaturatedY.append(y)
     if (len(unSaturatedX) == len(unSaturatedY) == 0):  # if both list is empty return true
@@ -162,19 +263,20 @@ def isPerfectMatch(g):
     return (perfect, unSaturatedX, unSaturatedY)
 
 
-# input List - needs to find way to input.
-inputAdjacencyList = [["a", "b", "c"], ["a", "b"], ["a", "l", "s"], ["l"], ["a", "l"]]
-g = createBipartiteGraph(inputAdjacencyList)
-for item in g.adj.items():
-    print(item)
-# print(g.graph['y'])
-# top = nx.bipartite.sets(g)[0]
-# pos = nx.bipartite_layout(g,top)
-# colors = [g[u][v]['color'] for u,v in g.edges()]
-# print(colors)
-# nx.draw(g,pos,with_labels = True,edge_color = colors )
-# nx.draw_networkx_edges(g,pos)
+if __name__ == '__main__':
+    # input List - needs to find way to input.
+    inputAdjacencyList = [["a", "b", "c"], ["a", "b"], ["a", "d", "e"], ["d"], ["a", "d"]]
+    g = createBipartiteGraph(inputAdjacencyList)
+    for item in g.adj.items():
+        print(item)
+    # print(g.graph['y'])
+    # top = nx.bipartite.sets(g)[0]
+    # pos = nx.bipartite_layout(g,top)
+    # colors = [g[u][v]['color'] for u,v in g.edges()]
+    # print(colors)
+    # nx.draw(g,pos,with_labels = True,edge_color = colors )
+    # nx.draw_networkx_edges(g,pos)
 
-# print(g.edges())
-# print(list(g.nodes(data=True)))
-hungarianAlgo(g)  # start algorithm
+    # print(g.edges())
+    # print(list(g.nodes(data=True)))
+    HungarianAlgorithm(g).start()  # start algorithm
